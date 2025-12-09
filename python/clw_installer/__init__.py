bd42a47b03f9e2654b641acd11b079076da5848d
sshu2017 <sshu2017@yahoo.com>
2025-12-09 15:29:27 -0800

    v0.1.8

--- python/clw_installer/__init__.py
+++ python/clw_installer/__init__.py
@@ -5,7 +5,7 @@ This is a Python wrapper that downloads and manages the pre-built
 Rust binary for your platform.
 """
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 __author__ = "CLW Contributors"
 __license__ = "MIT"
 
